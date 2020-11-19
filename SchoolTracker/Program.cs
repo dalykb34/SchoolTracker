ecbee73766825c2d9a53464836a34e5b3b9da21d
dalykb34 <dalykb34@gmail.com>
2020-11-19 15:14:17 -0600

    first commit

---
+++ SchoolTracker/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SchoolTracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var studentsGrades = new int[] { 80, 77, 87, 87, 65, 98, 70, 55, 92, 83, 39 };
+
+            foreach (var student in studentsGrades)
+            {
+                Console.WriteLine(student);
+            }
+            
+
+        }
+    }
+}
